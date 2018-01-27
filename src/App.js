74e698cbe4613f699ca7fd921c92e8dc4e2f0fe8
Tremelune <w@quieteyes.com>
2018-01-26 18:17:55 -0800

    - Pulled buildRpmRows() out of buildGearSection() for clarity. - Moved calculateSpeed() to bottom of class (classes are clearer when they go from general to specific from the top down).

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  // I pulled this jank math from:
-  // http://www.hotrod.com/articles/speed-rpm-gear-ratio-tire-size-formula/
-  calculateSpeed(tireDiameter, finalDrive, gearRatio, rpm) {
-    const speed = (rpm * tireDiameter) / (finalDrive * gearRatio * 336.13)
-    return Math.round(speed);
-  }
-
   render() {
     const tireDiameter = 26; // Inches
     const finalDrive = 3.31;
@@ -37,17 +30,30 @@ class App extends Component {
 
     var index = 1;
     for (let gear of gears) {
-      var rpmRows = [];
-      for (let rpm of rpms) {
-        let speed = this.calculateSpeed(tireDiameter, finalDrive, gear, rpm);
-        rpmRows.push(<ol>{rpm}rpm: {speed}mph</ol>)
-      }
+      var rpmRows = this.buildRpmRows(tireDiameter, finalDrive, gear, rpms);
 
       gearSections.push(<div>Gear {index++} ({gear})</div>)
       gearSections.push(rpmRows);
     }
 
     return gearSections;
+  }
+
+  buildRpmRows(tireDiameter, finalDrive, gear, rpms) {
+    var rpmRows = [];
+    for (let rpm of rpms) {
+      let speed = this.calculateSpeed(tireDiameter, finalDrive, gear, rpm);
+      rpmRows.push(<ol>{rpm}rpm: {speed}mph</ol>)
+    }
+
+    return rpmRows
+  }
+
+  // I pulled this jank math from:
+  // http://www.hotrod.com/articles/speed-rpm-gear-ratio-tire-size-formula/
+  calculateSpeed(tireDiameter, finalDrive, gearRatio, rpm) {
+    const speed = (rpm * tireDiameter) / (finalDrive * gearRatio * 336.13)
+    return Math.round(speed);
   }
 }
 
