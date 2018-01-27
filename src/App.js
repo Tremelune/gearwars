e8574305993114c6e7b43bcaca80d753db800964
Tremelune <w@quieteyes.com>
2018-01-26 17:51:07 -0800

    Get some dummy data displayed.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // const finalDrive = 3.31;
+    // const gears = [4.236, 2.538, 1.665, 1.238, ]
+
+    var rpms = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+
+    var gearSections = [];
+    for (let i=1; i<9; i++) {
+      var rpmRows = [];
+      for (let rpm of rpms) {
+        rpmRows.push(<ol>{rpm}rpm: 0 mph</ol>)
+      }
+
+      gearSections.push(<div>Gear {i}</div>) //todo Fuckin' BRs.
+      gearSections.push(rpmRows);
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +27,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {gearSections}
         </p>
       </div>
     );
