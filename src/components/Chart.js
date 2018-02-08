87e6433883d734b6362818a15ae786c8e13fe09c
Tremelune <w@quieteyes.com>
2018-02-08 13:55:18 -0800

    Moved stuff from Chart and ChartDataWrangler into ChartRenderer.
    There was an opportunity to pull some stuff out of the Chart component, and the ChartDataWrangler was no longer named well as a result.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {LineChart} from 'react-easy-chart';
-import ChartDataWrangler from '../biz/ChartDataWrangler.js';
-import * as LineColoration from '../biz/LineColoration.js';
+import ChartRenderer from '../biz/ChartRenderer.js';
 
 /** Uses this library: https://rma-consulting.github.io/react-easy-chart/line-chart/index.html */
 class Chart extends Component {
@@ -19,17 +18,18 @@ class Chart extends Component {
    *     },
    *   ]
    * }
+   *
+   * todo should be {drivetrains: []}
    */
   constructor(props) {
     super();
-
     this.state = this.calculateDimensions();
   }
 
 
   render() {
-    let lineColors = this.generateLineColors(this.props)
-    let data = this.buildDataFromDrivetrains(this.props)
+    let lineColors = ChartRenderer.generateLineColors(this.props.drivetrains);
+    let data = ChartRenderer.buildDataFromDrivetrains(this.props.drivetrains);
     return (
       <LineChart
         grid
@@ -61,46 +61,8 @@ class Chart extends Component {
     this.setState(this.calculateDimensions());
   }
 
-
-  /** Generates line colors. One gradient per drivetrain. One color per gear. */
-  generateLineColors(props) {
-    // This seems a bit weird, but I didn't know how to append in a map().
-    let gradients = [];
-    props.drivetrains.map((drivetrain, index) => {
-      let count = drivetrain.gearRatios.length;
-      let gradient = LineColoration.generateGradient(index, count)
-      gradients = [...gradients, ...gradient];
-      return null;
-    })
-    return gradients;
-  }
-
-  /** Converts drivetrains to chart data, and combines them in a single array. */
-  buildDataFromDrivetrains(props) {
-    // This seems a bit weird, but I didn't know how to append in a map().
-    let combinedData = [];
-    props.drivetrains.map((drivetrain, index) => {
-      let data = ChartDataWrangler.toData(drivetrain);
-      combinedData = [...combinedData, ...data];
-      return null;
-    })
-    return combinedData;
-  }
-
   calculateDimensions() {
-    let width = window.innerWidth;
-    let height = Math.round(width * 9 / 16); // Arbitrary aspect ratio...
-
-    let reasonableMaxHeight = window.innerHeight * 0.8;
-    if(height > reasonableMaxHeight) {
-      height = reasonableMaxHeight;
-      width = Math.round(height * 16 / 9);
-    }
-
-    return {
-      width: width,
-      height: height,
-    }
+    return ChartRenderer.calculateDimensions(window.innerWidth, window.innerHeight)
   }
 }
 
