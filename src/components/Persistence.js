a9ae46876d4e6978d0920b900fe0c5abb793bc09
Tremelune <w@quieteyes.com>
2018-02-07 17:04:44 -0800

    Added UI clarity.

--- src/components/Persistence.js
+++ src/components/Persistence.js
@@ -5,9 +5,11 @@ class Persistence extends Component {
   render() {
     return (
       <div>
-        <button onClick={(e) => Persister.clear()}>Clear</button>
-        <button onClick={(e) => this.revert()}>Revert</button>
+        <button onClick={(e) => Persister.clear()}>Clear Saved</button>
+        <button onClick={(e) => this.revert()}>Load Saved</button>
         <button onClick={(e) => Persister.save(this.props.drivetrains)}>Save</button>
+        <br />
+        <br />
       </div>
     );
   }
