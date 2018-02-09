67518c30da35d31d0240008094b9179b3afb5c5d
Tremelune <w@quieteyes.com>
2018-02-09 15:57:08 -0800

    Added test for generateLineColors().

--- src/__tests__/biz/ChartRenderer.test.js
+++ src/__tests__/biz/ChartRenderer.test.js
@@ -1,16 +1,26 @@
 import ChartRenderer from '../../biz/ChartRenderer.js';
 
+test('generate line colors', () => {
+  let drivetrains = [
+    {gearRatios: [4.236, 1]},
+    {gearRatios: [2.538, 1]},
+  ];
+
+  let expected = ["dd0000", "ffcccc", "0000dd", "ccccff"]; // todo Mock lineColoration.
+  let actual = ChartRenderer.generateLineColors(drivetrains);
+  expect(actual).toEqual(expected);
+});
+
+
 test('build data from drivetrains', () => {
   let drivetrains = [
     {
-      name: "Mustang",
       tireDiameter: 27.3,
       finalDrive: 3.31,
       gearRatios: [4.236],
       redline: 6800,
     },
     {
-      name: "Jeep",
       tireDiameter: 33,
       finalDrive: 4.11,
       gearRatios: [4.236, 2.538],
@@ -21,7 +31,7 @@ test('build data from drivetrains', () => {
   let expected = [
     [{"x": 0, "y": 0}, {"x": 39, "y": 6800}],
     [{"x": 0, "y": 0}, {"x": 35, "y": 6200}],
-    [{"x": 0, "y": 0}, {"x": 58, "y": 6200}],   
+    [{"x": 0, "y": 0}, {"x": 58, "y": 6200}],
   ];
 
   let actual = ChartRenderer.buildDataFromDrivetrains(drivetrains);
