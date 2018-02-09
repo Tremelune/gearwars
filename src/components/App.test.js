e4b5b667738154c6c091e03b6e4f24be39c9471f
Tremelune <w@quieteyes.com>
2018-02-09 11:13:20 -0800

    AND AWAY WE GO

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Okay...Now what do I do?
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
