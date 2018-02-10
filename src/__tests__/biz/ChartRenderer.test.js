3f78ce429fc8cca8f27ad5b8bfca7dcbf58a4627
Tremelune <w@quieteyes.com>
2018-02-09 17:17:00 -0800

    Using mocks for LineColoration in ChartRenderer.
    An order of magnitude simpler using dependency injection than Jests manual mocking.

--- src/__tests__/biz/ChartRenderer.test.js
+++ src/__tests__/biz/ChartRenderer.test.js
@@ -6,8 +6,9 @@ test('generate line colors', () => {
     {gearRatios: [2.538, 1]},
   ];
 
-  let expected = ["dd0000", "ffcccc", "0000dd", "ccccff"]; // todo Mock lineColoration.
-  let actual = ChartRenderer.generateLineColors(drivetrains);
+  let underTest = new ChartRenderer(new MockColoration());
+  let actual = underTest.generateLineColors(drivetrains);
+  let expected = ["000000", "ffffff", "000000", "ffffff"];
   expect(actual).toEqual(expected);
 });
 
@@ -38,14 +39,23 @@ test('build data from drivetrains', () => {
   expect(actual).toEqual(expected);
 });
 
+
 test('calculate dimensions', () => {
   let expected = {width: 800, height: 450};
   let actual = ChartRenderer.calculateDimensions(800, 800);
   expect(actual).toEqual(expected);
 });
 
+
 test('calculate dimensions constrains to height', () => {
   let expected = {width: 1138, height: 640};
   let actual = ChartRenderer.calculateDimensions(8000, 800);
   expect(actual).toEqual(expected);
 });
+
+
+class MockColoration {
+  generateGradient(index, count) {
+    return ['000000', 'ffffff'];
+  }
+}
