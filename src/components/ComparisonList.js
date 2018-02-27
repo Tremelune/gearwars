775c7e0010d8408e7bdc62b7dc1e83e8c2943f1e
Tremelune <w@quieteyes.com>
2018-02-27 15:31:42 -0800

    Added duplicate link...Also corrected CSS font size param...

--- src/components/ComparisonList.js
+++ src/components/ComparisonList.js
@@ -7,9 +7,10 @@ export default class ComparisonList extends Component {
       <div>
         <b>Saved Comparisons</b>
         {this.props.comparisons.map((comparison, index) =>
-          <div key={index}>
+          <div key={index} className="comparisonList">
             {comparison.name}
-            <a onClick={(e) => this.deleteComparison(comparison.id)}>(remove)</a>
+            <a onClick={(e) => this.duplicate(comparison)}>(duplicate)</a>
+            <a onClick={(e) => this.delete(comparison.id)}>(remove)</a>
           </div>
         )}
         <br />
@@ -18,7 +19,14 @@ export default class ComparisonList extends Component {
   }
 
 
-  deleteComparison(id) {
+  duplicate(comparison) {
+    // The presence of an ID is what switches the update vs create logic in save...I dunno if it belongs here.
+    comparison.id = null;
+    locator.comparisonDao.save(comparison);
+    this.props.reloadSavedComparisons();
+  }
+
+  delete(id) {
     locator.comparisonDao.delete(id);
     this.props.reloadSavedComparisons();
   }
