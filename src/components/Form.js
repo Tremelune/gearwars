df85d25807481d0cbcfe215bf578ec9a46e5db9e
William Benedict <will@benedict.wtf>
2022-02-01 22:02:42 -0800

    Better layout of drivetrain forms for mobile

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,22 +24,43 @@ export default class Form extends Component {
           Name: <input name="name" type="text" value={this.state.name} onChange={onChange} />
         </div>
         <div>
-          Tire Diameter: <input name="tireDiameter" type="number" value={this.state.tireDiameter} onChange={onChange} />
+          Tire Diameter: <input className="drivetrainInput" name="tireDiameter" type="number" value={this.state.tireDiameter} onChange={onChange} />
         </div>
         <div>
-          Final Drive: <input name="finalDrive" type="number" value={this.state.finalDrive} onChange={onChange} />
+          Final Drive: <input className="drivetrainInput" name="finalDrive" type="number" value={this.state.finalDrive} onChange={onChange} />
         </div>
-        <div>Redline: <input name="redline" type="number" value={this.state.redline} onChange={onChange} /></div>
+        <div>Redline: <input className="drivetrainInput"  name="redline" type="number" value={this.state.redline} onChange={onChange} /></div>
 
         {/* todo Make number of gears mutable. Some cars have like TEN... */}
-        <div>Gear Ratios</div>
-        <div>First: <input name="gear0" type="number" value={this.state.gear0} onChange={onChange} /></div>
-        <div>Second: <input name="gear1" type="number" value={this.state.gear1} onChange={onChange} /></div>
-        <div>Third: <input name="gear2" type="number" value={this.state.gear2} onChange={onChange} /></div>
-        <div>Fourth: <input name="gear3" type="number" value={this.state.gear3} onChange={onChange} /></div>
-        <div>Fifth: <input name="gear4" type="number" value={this.state.gear4} onChange={onChange} /></div>
-        <div>Sixth: <input name="gear5" type="number" value={this.state.gear5} onChange={onChange} /></div>
-        <div>Seventh: <input name="gear6" type="number" value={this.state.gear6} onChange={onChange} /></div>
+        <div className="gearRatios">
+          <div>Gear Ratios</div>
+          <table>
+            <tr>
+              <td>1</td>
+              <td><input className="drivetrainInput" name="gear0" type="number" value={this.state.gear0} onChange={onChange} /></td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td><input className="drivetrainInput" name="gear1" type="number" value={this.state.gear1} onChange={onChange} /></td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td><input className="drivetrainInput" name="gear2" type="number" value={this.state.gear2} onChange={onChange} /></td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td><input className="drivetrainInput" name="gear3" type="number" value={this.state.gear3} onChange={onChange} /></td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td><input className="drivetrainInput" name="gear4" type="number" value={this.state.gear4} onChange={onChange} /></td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td><input className="drivetrainInput" name="gear5" type="number" value={this.state.gear5} onChange={onChange} /></td>
+            </tr>
+          </table>
+        </div>
       </form>
     );
   }
