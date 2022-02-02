31559d56c5a2ac7ff0937dc9028336432e51c3b5
William Benedict <will@benedict.wtf>
2022-02-02 10:33:25 -0800

    Fixed tests from color order change...

--- src/__tests__/biz/LineColoration.test.js
+++ src/__tests__/biz/LineColoration.test.js
@@ -3,13 +3,13 @@ import LineColoration from '../../biz/LineColoration.js';
 let underTest = new LineColoration();
 
 test('base case', () => {
-  let expected = ['#dd0000', '#ffcccc'];
+  let expected = ['#0000dd', '#ccccff'];
   let actual = underTest.generateGradient(0,2);
   expect(actual).toEqual(expected);
 });
 
 test('interpolation', () => {
-  let expected = ['#0000dd', '#6666ee', '#ccccff'];
+  let expected = ['#dd0000', '#ee6666', '#ffcccc'];
   let actual = underTest.generateGradient(1,3);
   expect(actual).toEqual(expected);
 });
